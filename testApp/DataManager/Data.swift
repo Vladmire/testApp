56813fd1ab606289ca2633e8061e6c4ff071e720
Vladimir Dubovik <dubo@sfedu.ru>
2022-05-24 15:08:28 +0300

    first commit

---
+++ testApp/DataManager/Data.swift
@@ -0,0 +1,8 @@
+//
+//  Data.swift
+//  testApp
+//
+//  Created by imac44 on 24.05.2022.
+//
+
+import Foundation
---
+++ testApp/DataManager/dataAPI.swift
@@ -0,0 +1,8 @@
+//
+//  dataAPI.swift
+//  testApp
+//
+//  Created by imac44 on 24.05.2022.
+//
+
+import Foundation
